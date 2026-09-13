e90f7886be3a6f42a517626635bdc3d4a8644d44
gargaditya254-dev <gargaditya254@gmail.com>
2026-09-14 00:47:20 +0530

    Create code.c

---
+++ day14_que1/code.c
@@ -0,0 +1,19 @@
+Write a program to print the sum of the first n odd numbers.
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+    int odd = 1;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum = sum + odd;
+        odd = odd + 2;
+    }
+
+    printf("Sum of first %d odd numbers = %d\n", n, sum);
+
+    return 0;
+}
