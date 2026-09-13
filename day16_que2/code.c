c96b48b9bf628ee046f1cd4a07f2bd10e5690cbe
gargaditya254-dev <gargaditya254@gmail.com>
2026-09-14 01:00:47 +0530

    Create code.c
    Implement a program to check for palindrome numbers.

---
+++ day16_que2/code.c
@@ -0,0 +1,25 @@
+Write a program to check if a number is a palindrome.
+  #include <stdio.h>
+
+int main() {
+    int n, original, reverse = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n = n / 10;
+    }
+
+    if (original == reverse) {
+        printf("%d is a palindrome\n", original);
+    } else {
+        printf("%d is not a palindrome\n", original);
+    }
+
+    return 0;
+}
