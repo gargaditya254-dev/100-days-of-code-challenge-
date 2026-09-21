137699d351ecf6ae744650b0463d964a23883681
gargaditya254-dev <gargaditya254@gmail.com>
2026-09-21 23:27:12 +0530

    Create code.c

---
+++ day28_que2/code.c
@@ -0,0 +1,26 @@
+Read and print elements of a one-dimensional array.
+  
+#include <stdio.h>
+
+int main() {
+    int n, arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are:\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
