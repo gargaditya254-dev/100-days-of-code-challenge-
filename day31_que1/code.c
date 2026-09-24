9224630694f149a698a2d094228729ea510e697f
gargaditya254-dev <gargaditya254@gmail.com>
2026-09-24 23:15:45 +0530

    Create code.c

---
+++ day31_que1/code.c
@@ -0,0 +1,33 @@
+Search for an element in an array using linear search.
+  #include <stdio.h>
+
+int main() {
+    int n, arr[100], search;
+    int found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &search);
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == search) {
+            printf("Element found at position %d\n", i + 1);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
